dd1cf326779203b6ff7bb1e41e6f1f0586500fe0
554291911 <554291911@qq.com>
2017-08-10 15:46:37 +0800

    9. Palindrome Number

---
+++ src/No4_PalindromeNumber.java
@@ -0,0 +1,19 @@
+public class No4_PalindromeNumber {
+	public boolean isPalindrome(int x) {
+		if (x < 0)
+			return false;
+		else {
+			char[] t = (x + "").toCharArray();
+			for (int i = 0; i < t.length / 2; i++) {
+				if (t[i] != t[t.length - 1 - i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new No4_PalindromeNumber().isPalindrome(12321));
+	}
+}
